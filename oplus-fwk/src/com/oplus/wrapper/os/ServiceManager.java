fa43c86ae28208d6efb8403218f85cff2880ebae
chandu078 <chandudyavanapelli03@gmail.com>
2024-04-09 16:38:47 +0530

    oplus-fwk: Import few more oplus classes
    Signed-off-by: mvimal2607 <mvimal2607@gmail.com>

--- oplus-fwk/src/com/oplus/wrapper/os/ServiceManager.java
+++ oplus-fwk/src/com/oplus/wrapper/os/ServiceManager.java
@@ -2,7 +2,6 @@ package com.oplus.wrapper.os;
 
 import android.os.IBinder;
 
-/* loaded from: classes.dex */
 public class ServiceManager {
     public static IBinder checkService(String name) {
         return android.os.ServiceManager.checkService(name);
